13b55149bbb0567492d26b94c53d81363d2e1217
amagin0ichi006 <alaminrabiu50@gmail.com>
2025-01-19 00:33:05 -0600

    eliminated r commentedundant
    i removed a comment that served no purpose whatsoever

--- forms.py
+++ forms.py
@@ -10,13 +10,6 @@ class SignUpForm(UserCreationForm):
         model = User
         fields = ['username','email','password1', 'password2']
 
-    # def __init__(self, *args: Any, **kwargs: Any):
-    #     super(SignUpForm, self).__init__(*args, **kwargs)
-
-
-    #     for fieldname in ['username','email','password1', 'password2']:
-    #         self.fields[fieldname].help_text = None
-        
 class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
